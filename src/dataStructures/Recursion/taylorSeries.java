e33df8580853f0bb9e75b62d2681fc79b469cde9
Varma Aniket <vaan.origin@gmail.com>
2023-05-30 17:32:27 +0530

    update taylorSeries.java with new functions and new line of comments

--- src/dataStructures/Recursion/taylorSeries.java
+++ src/dataStructures/Recursion/taylorSeries.java
@@ -27,14 +27,17 @@ public class taylorSeries {
     static double pow = 1;
     static double fact = 1;
 
-    public static double taylorBase(double x,double n) {
+    //time ---> O(n^2) no. of multiplication in taylor series
+    // Space ----> O(n) for taylorBasic1
+    //Basic Taylor using recursion
+    public static double taylorBasic1(double x,double n) {
         double res ;
         if (n==0){
             res = 1 ; // since the first term is 1 in taylor series
            return res;
         }
         else{
-            res = taylorBase(x,n-1);
+            res = taylorBasic1(x,n-1);
 
             // EXECUTION DURING RETURNING TIME IN RECURSION TREE
             taylorSeries.pow = taylorSeries.pow * x; // P = P * X(POWER-VARIABLE)
@@ -44,10 +47,56 @@ public class taylorSeries {
             return res + taylorSeries.pow/taylorSeries.fact;
         }
     }
+
+    //Basic Taylor using Iterative or Loop
+    //time ---> O(n^2) no. of multiplication in taylor series
+    // Space ----> O(n) for taylorBasic2
+    public static double taylorBasic2(double x, double n) {
+        double S = 1;
+        double den = 1;
+        double num = 1;
+        for (int i = 1; i <=n; i++) {
+             num = num * x;     // P = P * N
+             den = den * i;
+             S = S + num/den ;    // (S + x/n) here S = 1 ;hence (1+ x/n)
+
+        }
+        return S;
+    }
+    
+    // Using Horner's Rule Recursion
+    // Time ----> O(n) no. of multiplication in taylor series
+    // Space ---> O(n)
+    static double S=1;
+    public static double taylorHornerRecursion(double x ,double n){
+        if (n == 0) {
+            return S;
+        }
+        else {
+            S = 1 + x/n * S;
+            return taylorHornerRecursion(x,n-1);
+        }
+
+    }
+
+
+    // Using Horner's Rule in Iterative or Loop
+    // Time ----> O(n) no. of multiplication in taylor series
+    // Space ---> O(1)
+    public static double taylorHornerLoop(double x , double n) {
+        double S = 1;
+        for (;n>0;n--) {
+            S = 1 + x / n * S;
+        }
+        return S;
+    }
     public static void main(String[] args) {
         double x = 1;
         double n = 10;
-        System.out.println(taylorBase(x,n));
+        System.out.println(taylorBasic1(x,n));
+        System.out.println(taylorBasic2(x,n));
+        System.out.println(taylorHornerRecursion(x,n));
+        System.out.println(taylorHornerLoop(x,n));
 
     }
 }
